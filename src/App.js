1d94142bc2961ebe5bebb2c06471bf878b9a3fb3
Henry <hchen651@gmail.com>
2019-10-08 17:56:12 -0400

    updated layout

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from "./components/Navbar";
+import MainContainer from "./components/MainContainer";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <MainContainer />
+    </div>
+  );
+}
+
+export default App;
